b234d122359f0cf4c06263a1987d43ad6761d938
Francisco-Zeferino <franciscozeferino06@gmail.com>
2025-01-27 20:54:07 +0000

    Adding communication with various endpoints

--- API/mysite/api/endpoints/views.py
+++ API/mysite/api/endpoints/views.py
@@ -1,28 +1,32 @@
-import json
+import json, requests
 from django.shortcuts import render
 from rest_framework import generics, status
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-
+from django.urls import reverse
 
 #REMOVE!!!!!!! This is here because of csrf cookies, temp for api endpoint testing  
 @csrf_exempt
 def starter_view(request):
-    print("-------------Request---------------")
-    print(request)
-    print("-----------------------------------")
+    mockData = {
+        'name': 'Ze',
+        'auth': True,
+        'admin': False
+    }
     if request.method == "GET":
-        mockData = {
-            'name': 'Ze',
-            'auth': True,
-            'admin': False
-        }
-        return JsonResponse(mockData, status=status.HTTP_200_OK)
+        url = "http://127.0.0.1:8000/chat_view/"
+        response = requests.get(url)
+        if response.status_code == 200:
+            return HttpResponse(response.content)
+            #return JsonResponse(mockData, status=status.HTTP_200_OK)
     elif request.method == "POST":
         jsonData = json.loads(request.body)
-        name = jsonData.get('name')     
+        name = jsonData.get('name')
+        date = jsonData.get('date')
         mockDatas = {
             'name' : name,
-            'test' : "123456789"
+            'date' : date
         }
         return JsonResponse(mockDatas, status=status.HTTP_201_CREATED)
+    else:
+        return HttpResponse(status=status.HTTP_400_BAD_REQUEST)
--- API/mysite/api/urls.py
+++ API/mysite/api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 # from .endpoints import test_view
 from .endpoints import views
+from .endpoints import chat_view
 
 urlpatterns = {
     path("index/", views.starter_view, name="starter_view-cre"),
+    path("chat_view/", chat_view.chat, name="chat_view_name")
 }
