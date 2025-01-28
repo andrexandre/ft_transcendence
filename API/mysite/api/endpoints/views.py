11713a4466e6b64d17be3f7fa0f975fd743dc09b
Francisco-Zeferino <franciscozeferino06@gmail.com>
2025-01-28 16:02:22 +0000

    TODO: API auth

--- API/mysite/api/endpoints/views.py
+++ API/mysite/api/endpoints/views.py
@@ -2,17 +2,27 @@ import json, requests
 from django.shortcuts import render
 from rest_framework import generics, status
 from django.http import HttpResponse, JsonResponse
+from rest_framework.views import APIView
 from django.views.decorators.csrf import csrf_exempt
 from django.urls import reverse
+from django.core import serializers
 
-#REMOVE!!!!!!! This is here because of csrf cookies, temp for api endpoint testing  
+#REMOVE!!!!!!! This is here because of csrf cookies, temp for api endpoint testing 
 @csrf_exempt
 def starter_view(request):
-    mockData = {
-        'name': 'Ze',
-        'auth': True,
-        'admin': False
-    }
+    mockData = [
+        {
+            'name': 'Ze',
+            'auth': True,
+            'admin': False
+        },
+        {
+            'name': 'Zecas',
+            'auth': False,
+            'admin': False
+        }
+    ]
+    return JsonResponse({"items" : mockData}, status=status.HTTP_200_OK, safe=False)
     if request.method == "GET":
         url = "http://127.0.0.1:8000/chat_view/"
         response = requests.get(url)
