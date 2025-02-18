c06788363cf073748210068dfc3f0e5c9cb3e036
aleperei <134283833+mouracv@users.noreply.github.com>
2025-02-18 16:13:51 +0000

    testing db

--- backend/user/userManagement/database_plugin.js
+++ backend/user/userManagement/database_plugin.js
@@ -1,19 +1,22 @@
-import FastifyPlugin from 'fastify-plugin';
+import fp from 'fastify-plugin';
 import sqlite3 from 'sqlite3';
 
-const FILEPATH = './database/user.db';
+// const FILEPATH = './user.db';
+// const FILEPATH = './database/user.db';
+
 
 
 function fastifySqlite(fastify, options, done) {
 
 	const modes = sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE;
-	const connection = new sqlite3.Database(FILEPATH, modes, (err) => {
+	const connection = new (sqlite3.verbose().Database)(( options.dbPath || ":memory:"), modes, (err) => {
     if (err) {
       console.log('Failed to connect to database!');
-    }
-    console.log('Sucessful connection to database!');
-  }).verbose();
-
+    } else {
+		console.log('Sucessful connection to database!');
+	}
+  });
+  
   if (!fastify.sqlite) {
     fastify.decorate('sqlite', connection);
   }
@@ -23,4 +26,4 @@ function fastifySqlite(fastify, options, done) {
   done();
 }
 
-export default FastifyPlugin(fastifySqlite, { name: 'fastify-mysql-example' });
+export default fp(fastifySqlite, { dbPath: './user.db'});
--- backend/user/userManagement/server.js
+++ backend/user/userManagement/server.js
@@ -1,24 +1,44 @@
 import fastify from "fastify";
-// import fastifySqlite from './database_plugin.js'
+import fastifySqlite from './database_plugin.js'
 // import sqlite from 'node:sqlite'
 
 // Creation of the app  instance
 const server = fastify({logger: true});
 
-// server.register(fastifySqlite);
+server.register(fastifySqlite);
 
-server.get('/', (request, response) => {
-
-	response.header('content-type', 'application/json');
-	response.send({message: " Now we are working"});
+server.ready((err) => {
+	if (err) {
+		console.log('Nao funcionou');
+	} else {
+		console.log('funcionou');
+		server.sqlite.run("CREATE TABLE alex (info TEXT)");
+	}
 });
+// server.printPlugins();
 
 
-const listenOptions = {
-	port: `${process.env.PORT}`,
-	host: '0.0.0.0'
-}
+// console.log(server);
 
-server.listen(listenOptions, () => {
-	console.log(`Server is running on port: ${process.env.PORT}`);
-});
+
+// server.register(function plugin (app, opts, next) {
+// 	console.log('');
+// 	next()
+// });
+// console.log(server);
+
+// server.get('/', (request, response) => {
+
+// 	response.header('content-type', 'application/json');
+// 	response.send({message: " Now we are working"});
+// });
+
+
+// const listenOptions = {
+// 	port: `${process.env.PORT}`,
+// 	host: '0.0.0.0'
+// }
+
+// server.listen(listenOptions, () => {
+// 	console.log(`Server is running on port: ${process.env.PORT}`);
+// });
