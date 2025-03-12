1166686c432f520ab9873958ca7a74dfe3a3df93
andrexandre <106836082+andrexandre@users.noreply.github.com>
2025-03-12 19:35:10 +0000

    added user=antony to dashboard chat button

--- frontend/src/pages/dashboard.ts
+++ frontend/src/pages/dashboard.ts
@@ -12,7 +12,7 @@ class Dashboard extends Page {
 			window.location.href = "http://127.0.0.1:5000";
 		});
 		document.getElementById("chat-button")!.addEventListener("click", () => {
-			window.location.href = "http://127.0.0.1:2000";
+			window.location.href = "http://127.0.0.1:2000/?user=Antony";
 		});
 		assignButtonNavigation('settings-button', '/login');
 	}
