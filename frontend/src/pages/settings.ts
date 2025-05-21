065234cc162e726441e926a68b07491425f8f7b8
andrexandre <106836082+andrexandre@users.noreply.github.com>
2025-05-21 19:04:00 +0100

    2fa settings input

--- frontend/src/pages/settings.ts
+++ frontend/src/pages/settings.ts
@@ -110,13 +110,28 @@ class Settings extends Page {
 				}
 				if (twoFAButton.checked) {
 					const data = await response.json();
-					const imageElement = document.createElement('img');
-					imageElement.src = data.content;
-					imageElement.id = 'qr-code-img';
-					document.getElementById('col-1')?.appendChild(imageElement);
+					document.getElementById('2fa-info')!.outerHTML = /*html*/`
+						<div id="2fa-info" class="flex card pb-0 pt-2">
+							<img id="qr-code-img" src="${data.content}" class="shadow-lg shadow-neutral-400 rounded"/>
+							<div class="flex flex-col justify-center self-center gap-6 ml-20">
+								<p>
+									<b>To set up two factor authentication</b> <br>
+									1. Download an authenticator app. <br>
+									2. Scan the QR code. <br>
+									3. Enter the 2FA code from the app, here.
+								</p>
+								<input type="text" id="qr-code-input" class="p-1 t-dashed pl-4 h-fit" placeholder="Enter 2FA code" maxlength="6" />
+							</div>
+						</div>
+					`;
+					document.getElementById('qr-code-input')!.addEventListener('input', async (event) => {
+						const input = (event.target as HTMLInputElement);
+						if (input.value.length === 6)
+							lib.showToast(`Sent 2FA code: ${input.value}`);
+					});
 					lib.showToast.green("2FA enabled");
 				} else {
-					document.getElementById('qr-code-img')?.remove();					
+					document.getElementById('2fa-info')?.classList.add('hidden');
 					lib.showToast.red("2FA disabled");
 				}
 			} catch (error: any) {
@@ -164,7 +179,7 @@ class Settings extends Page {
 			${sidebar.getHtml()}
 			<main class="grid grid-cols-2 max-2xl:grid-cols-1 flex-1 card t-dashed text-start overflow-auto">
 				<div id="col-1">
-					<form class="card flex flex-col overflow-auto" action="#">
+					<form class="card flex flex-col overflow-auto py-0" action="#">
 						<h1 class="item text-start text-2xl">Profile</h1>
 						<div class="flex">
 							<button id="profile-image-button" type="button" class="relative size-60 group">
@@ -187,12 +202,15 @@ class Settings extends Page {
 						<textarea class="p-1 t-dashed pl-4" name="bio" id="profile-bio">Champion of belly rubs, fetch, and fierce squirrel chases. Sir Barkalot is the first to answer the doorbell with a royal bark. His hobbies include digging to China and chewing shoes.</textarea>
 						<button class="item t-dashed" type="submit">Save</button>
 					</form>
-					<div class="flex justify-between item">
-						<h1>2 Factor Authentication</h1>
-						<label class="h-7 w-12">
-							<input type="checkbox" class="sr-only peer" id="2fa-toggle">
-							<div class="h-full relative t-dashed peer peer-checked:after:translate-x-full after:absolute after:top-0.5 after:start-0.5 peer-checked:after:bg-c-text dark:peer-checked:after:bg-c-bg after:size-5 after:bg-c-secondary dark:after:bg-c-primary after:transition-all after:rounded-full"></div>
-						</label>
+					<div class="flex flex-col item">
+						<div class="flex justify-between item">
+							<h1>2 Factor Authentication</h1>
+							<label class="h-7 w-12">
+								<input type="checkbox" class="sr-only peer" id="2fa-toggle">
+								<div class="h-full relative t-dashed peer peer-checked:after:translate-x-full after:absolute after:top-0.5 after:start-0.5 peer-checked:after:bg-c-text dark:peer-checked:after:bg-c-bg after:size-5 after:bg-c-secondary dark:after:bg-c-primary after:transition-all after:rounded-full"></div>
+							</label>
+						</div>
+						<div id="2fa-info" class="hidden card pb-0 pt-2"></div>
 					</div>
 				</div>
 				<div class="flex flex-col">
@@ -250,7 +268,7 @@ class Settings extends Page {
 					const data = await response.json();
 					throw new Error(data.message);
 				}
-				lib.showToast.green("Updated!");
+				lib.showToast.green("Settings updated!");
 			} catch (error: any) {
 				return lib.showToast.red(error.message);
 			}
