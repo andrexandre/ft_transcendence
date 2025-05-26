0272940410623e06fd9b904dbd55d2cbf23cd1c3
fsantos2 <faria.francisco.santos@gmail.com>
2025-05-26 16:34:38 +0100

    done id syncing and invites to game persistence

--- frontend/src/pages/settings.ts
+++ frontend/src/pages/settings.ts
@@ -2,6 +2,7 @@ import Page from "./Page"
 import * as lib from "../utils"
 import sidebar from "../components/sidebar"
 import { setProfileImage } from "./dashboard";
+import { turnOffChat, turnOnChat } from "./chat/friends";
 
 const safeColors: string[] = ["bg-red-500", "bg-orange-500", "bg-amber-500", "bg-yellow-500", "bg-lime-500", "bg-green-500", "bg-emerald-500", "bg-teal-500", "bg-cyan-500", "bg-sky-500", "bg-blue-500", "bg-indigo-500", "bg-violet-500", "bg-purple-500", "bg-fuchsia-500", "bg-pink-500", "bg-rose-500", "bg-slate-500", "bg-gray-500", "bg-zinc-500", "bg-neutral-500", "bg-stone-500"];
 
@@ -268,6 +269,8 @@ class Settings extends Page {
 					const data = await response.json();
 					throw new Error(data.message);
 				}
+				turnOffChat();
+				turnOnChat();
 				lib.showToast.green("Settings updated!");
 			} catch (error: any) {
 				return lib.showToast.red(error.message);
