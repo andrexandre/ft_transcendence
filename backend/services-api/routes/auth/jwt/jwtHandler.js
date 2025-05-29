81db4d92f8d8e1ff083781f872975f8cef72925b
Francisco-Zeferino <franciscozeferino06@gmail.com>
2025-05-29 19:42:06 +0100

    added endpoint to generate token and add it to cookies

--- backend/services-api/routes/auth/jwt/jwtHandler.js
+++ backend/services-api/routes/auth/jwt/jwtHandler.js
@@ -1,14 +1,25 @@
-export default function jwtHandler(fastify, options){
-    fastify.post('/updateToken', async (request, reply) => {
-		const { username } = request.body;
-        const payload = await fastify.parseToReadableData(request.cookies.token);
-        payload.username = username;
-        const token =  await fastify.generateToken(payload);
-        reply.setCookie('token', token, {
-			      path: '/',
-            httpOnly: true,
-            secure: false,
-            sameSite: 'lax'
-		}).status(200);
-    })
+export default function jwtHandler(fastify, options) {
+  fastify.post('/updateToken', async (request, reply) => {
+    const { username } = request.body;
+    const payload = await fastify.parseToReadableData(request.cookies.token);
+    payload.username = username;
+    const token = await fastify.generateToken(payload);
+    reply.setCookie('token', token, {
+      path: '/',
+      httpOnly: true,
+      secure: false,
+      sameSite: 'lax'
+    }).status(200);
+  })
+
+  fastify.get('/generateToken', async (request, reply) => {
+    const payload = await fastify.prepareTokenData(response, "email");
+    const token = await fastify.generateToken(payload);
+    reply.status(200).setCookie("token", token, {
+      path: '/',
+      httpOnly: true,
+      secure: false,
+      sameSite: 'lax',
+    });
+  });
 }
