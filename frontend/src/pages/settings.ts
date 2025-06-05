8b1939dfbeeafa33757b1c82e3be6b9c1c054360
Francisco-Zeferino <franciscozeferino06@gmail.com>
2025-06-05 20:00:37 +0100

    Added jwt verification endpoint

--- frontend/src/pages/settings.ts
+++ frontend/src/pages/settings.ts
@@ -7,6 +7,15 @@ import { turnOffChat, turnOnChat } from "./chat/friends";
 const safeColors: string[] = ["bg-red-500", "bg-orange-500", "bg-amber-500", "bg-yellow-500", "bg-lime-500", "bg-green-500", "bg-emerald-500", "bg-teal-500", "bg-cyan-500", "bg-sky-500", "bg-blue-500", "bg-indigo-500", "bg-violet-500", "bg-purple-500", "bg-fuchsia-500", "bg-pink-500", "bg-rose-500", "bg-slate-500", "bg-gray-500", "bg-zinc-500", "bg-neutral-500", "bg-stone-500"];
 
 async function loadInformation() {
+	try{
+		const response = await fetch(`http://${location.hostname}:8080/token/verifyToken`, {
+			credentials: 'include'
+		});
+		if(response.status == 403)
+			throw Error;
+	} catch(error: any){
+		return lib.showToast.red("Unauthorized");
+	}
 	try {
 		const response = await fetch(`http://${location.hostname}:8080/api/users/settings`, {
 			credentials: 'include'
